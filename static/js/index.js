1e1163b352d8b98790f30605200b2d6a1f34f5f6
root <root@ResiUniDj1.0>
2015-11-16 10:18:53 -0500

    subiendo projecto

---
+++ static/js/index.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    /*$('#click').click(function(){
+        $.ajax({
+            url: '../../residencias',
+            dataType: 'json',
+            data:{
+                ciudad: 1
+            },
+            success: function(r){
+                alert(r.name_sede);
+            }
+        });
+    });*/
+
+});
